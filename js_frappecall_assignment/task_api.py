249e5bde52965820acbf3fc0404ea893947d7bbe
Moulis980 <vgmoulis24@gmail.com>
2026-08-17 11:28:32 +0530

    Add JS frappe.call assignment backend API

---
+++ js_frappecall_assignment/task_api.py
@@ -0,0 +1,15 @@
+import frappe
+
+
+@frappe.whitelist()
+def create_task(task_subject):
+    """Create a new Task with the supplied subject and return its name."""
+
+    if not task_subject:
+        frappe.throw("Task Subject is required")
+
+    task = frappe.new_doc("Task")
+    task.subject = task_subject
+    task.save()
+
+    return task.name
