46df1243e2767977e2c12a2194514543dd0c66cd
Moulis V G <vgmoulis24@gmail.com>
2026-09-08 16:47:55 +0530

    Add app metadata and fixtures to hooks.py

---
+++ Custom_button/hooks.py
@@ -0,0 +1,8 @@
+app_name = "practice_app"
+app_title = "Practice_app"
+app_publisher = "Moulis"
+app_description = "Practice"
+app_email = "vgmoulis24@gmail.com"
+app_license = "mit"
+
+fixtures = ["Client Script"]
