589d71a59337e6ed1b3baea67834f23dacf23d9c
Moulis V G <vgmoulis24@gmail.com>
2026-09-10 14:14:19 +0530

    Add test for Help Article creation

--- guide-3-assignment/test/.test_help_article.py
+++ guide-3-assignment/test/.test_help_article.py
@@ -1,1 +1,22 @@
+import frappe
 
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHelpArticle(FrappeTestCase):
+
+    def test_article_creation(self):
+        article = frappe.get_doc({
+            "doctype": "Help Article",
+            "title": "My First Test1",
+            "category": "Moulis",
+            "published": 1,
+            "content": "This is my first test article."
+        })
+
+        article.insert()
+
+        self.assertEqual(article.title, "My First Test1")
+        self.assertTrue(
+            frappe.db.exists("Help Article", article.name)
+        )
