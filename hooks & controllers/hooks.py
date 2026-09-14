bc1e5a58406a84836da1f800038491418f7c3025
Moulis V G <vgmoulis24@gmail.com>
2026-09-14 11:10:53 +0530

    Add hooks.py for application configuration

---
+++ hooks & controllers/hooks.py
@@ -0,0 +1,14 @@
+app_name = "practice_app"
+app_title = "Practice_app"
+app_publisher = "Moulis"
+app_description = "Practice"
+app_email = "vgmoulis24@gmail.com"
+app_license = "mit"
+
+export_python_type_annotations = True
+
+doc_events = {
+    "User": {
+        "validate": "practice_app.api3.custom_logic"
+    }
+}
