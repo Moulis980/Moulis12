f2776bfec9be3a4a0417f0ae34bce3927c5f1276
Moulis V G <vgmoulis24@gmail.com>
2026-09-14 14:02:46 +0530

    Update articles.py

--- guide-3-assignment/articles.py
+++ guide-3-assignment/articles.py
@@ -1,1 +1,8 @@
+import frappe
 
+def get_context(context):
+    context.articles = frappe.get_all(
+        "Help Article",
+        filters={"published": 1},
+        fields=["name", "title"]
+    )
